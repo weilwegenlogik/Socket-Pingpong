c0e827aeca82a99fb7b82826150f7e2cdc511463
Jayy <31707578+weilwegenlogik@users.noreply.github.com>
2023-11-10 15:06:57 +0100

    changes they might drive you insane but its killin

---
+++ TCPServer/TCPServer.py
@@ -0,0 +1,21 @@
+import socket
+
+# create a TCP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to a specific address and port
+server_address = ('localhost', 12000)
+sock.bind(server_address)
+
+while True:
+    print('waiting for message...')
+    data, address = sock.recvfrom(4096)
+
+    print(f'received {len(data)} bytes from {address}')
+    print(f'data: {data.decode()}')
+
+    # send a response back to the client
+    message = 'pong'
+    sock.sendto(message.encode(), address)
+
+    print(f'sent {len(message)} bytes back to {address}')
---
+++ UDPServer/UDPClient.py
@@ -0,0 +1,18 @@
+import socket
+
+# Set up the socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set the server address and port
+server_address = ('localhost', 12000)
+
+# Send the ping request
+message = b'ping'
+client_socket.sendto(message, server_address)
+
+# Receive the response
+data, server = client_socket.recvfrom(1024)
+print(f'Received "{data.decode()}" from {server}')
+
+# Close the socket
+client_socket.close()
