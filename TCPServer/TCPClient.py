892816b0df9a49cd60f3147ecf10a2d5be0ff0d9
Jayy <31707578+weilwegenlogik@users.noreply.github.com>
2023-11-10 15:07:21 +0100

    changes

--- TCPServer/TCPClient.py
+++ TCPServer/TCPClient.py
@@ -1,18 +1,24 @@
 import socket
 
-# Set up the socket
+# Define the server address and port
+SERVER_ADDRESS = 'localhost'
+SERVER_PORT = 12000
+
+# Create a TCP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Set the server address and port
-server_address = ('localhost', 12000)
+# Connect to the server
+client_socket.connect((SERVER_ADDRESS, SERVER_PORT))
 
 # Send the ping request
-message = b'ping'
-client_socket.sendto(message, server_address)
+ping_request = b'ping'
+client_socket.sendall(ping_request)
 
 # Receive the response
-data, server = client_socket.recvfrom(1024)
-print(f'Received "{data.decode()}" from {server}')
+response = client_socket.recv(1024)
+
+# Print the response
+print(response.decode())
 
 # Close the socket
 client_socket.close()
--- TCPServer/TCPServer.py
+++ TCPServer/TCPServer.py
@@ -1,21 +1,17 @@
 import socket
 
-# create a TCP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 12000        # The port used by the server
 
-# bind the socket to a specific address and port
-server_address = ('localhost', 12000)
-sock.bind(server_address)
-
-while True:
-    print('waiting for message...')
-    data, address = sock.recvfrom(4096)
-
-    print(f'received {len(data)} bytes from {address}')
-    print(f'data: {data.decode()}')
-
-    # send a response back to the client
-    message = 'pong'
-    sock.sendto(message.encode(), address)
-
-    print(f'sent {len(message)} bytes back to {address}')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            if data.decode() == "ping":
+                conn.sendall(b"pong")
